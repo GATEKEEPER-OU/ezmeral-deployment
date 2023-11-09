a8ce1242f84a6a17d9e0ae39f047f614fbbbc6f2
Riccardo Pala <rppala3@gmail.com>
2023-11-09 23:57:45 +0100

    Silly changes

--- getjdk-yum.sh
+++ getjdk-yum.sh
@@ -6,7 +6,7 @@ export http_proxy=$PROXY_URL
 export https_proxy=$PROXY_URL
 
 # Install Java 17 Corretto
-yum -y update
+# yum -y update
 rpm --import https://yum.corretto.aws/corretto.key
 curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
 yum install -y java-17-amazon-corretto-devel
